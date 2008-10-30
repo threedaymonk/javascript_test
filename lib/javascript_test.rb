794274ff9468f32d7d84f87ea590ac30171cf121
Tom Lea <commit@tomlea.co.uk>
2008-10-30 12:44:01 +0000

    Firefox does not seem to answer the phone to applescript.

--- lib/javascript_test.rb
+++ lib/javascript_test.rb
@@ -39,7 +39,7 @@ class JavaScriptTest
     end
   
     def visit(url)
-      applescript('tell application "Firefox" to Get URL "' + url + '"') if macos? 
+      system("open -b 'org.mozilla.firefox' '#{url}'") if macos? 
       system("#{@path} #{url}") if windows? 
       system("firefox #{url}") if linux?
     end
