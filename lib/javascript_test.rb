10393645b2c0f4d17531ca7ae20cde3f989ccc61
Tom Lea <commit@tomlea.co.uk>
2008-10-30 16:10:56 +0000

    Try and open IE in the background, even if it is going to ignore us.

--- lib/javascript_test.rb
+++ lib/javascript_test.rb
@@ -95,7 +95,7 @@ class JavaScriptTest
     def visit(url)
       system("#{@path} #{url}") if windows?
       url = url.gsub('http://localhost:4711/results', 'http%3A%5C%5Clocalhost%3A4711%5Cresults')
-      system("open -b 'com.codeweavers.CrossOverHelper.win98.Internet Explorer' '#{url}'") if macos?
+      system("open -g -b 'com.codeweavers.CrossOverHelper.win98.Internet Explorer' '#{url}'") if macos?
     end
   
     def to_s
